95b122fe21836d7662544c093bead8ba11360f10
gamer05 <gamer05@list.ru>
2019-11-14 17:05:32 +0300

    Размечена страница создания теста.

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,12 +1,105 @@
 import React, { Component } from 'react';
-//import classes from './QuizCreator.module.css';
+import classes from './QuizCreator.module.css';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+import {createControl} from '../../form/form';
 
+// ускоряем подготовку вариантов ответа
+// правильно ли, что всегда обязательно 4 варианта ответа?
+function createOptionControl(num) {
+    return createControl({
+        label: `Вариант ${num}`,
+        errorMessage: 'Значение не может быть пустым',
+        id: num,
+    },
+    {required: true})
+}
+
+// очистка параметров формы
+function createFormControls() {
+    return {
+        question: createControl({
+            label: 'Введите вопрос',
+            errorMessage: 'Вопрос не может быть пустым'
+        }, {required: true}),
+        option1: createOptionControl(1),
+        option2: createOptionControl(2),
+        option3: createOptionControl(3),
+        option4: createOptionControl(4),
+    }
+}
 
 export default class QuizCreator extends Component {
+    state = {
+        quiz: [],
+        formControls: createFormControls(),
+    }
+
+    submitHandler = e => {
+        e.preventDefault();
+    }
+
+    addQuestionHandler = () => {
+
+    }
+
+    createQuizHandler = () => {
+
+    }
+
+    changeHandler = (value, controlName) => {
+
+    }
+
+    renderInputes() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+
+            return (
+                <>
+                    <Input
+                        key={controlName + index}
+                        value={control.value}
+                        valid={control.valid}
+                        touched={control.touched}
+                        label={control.label}
+                        shouldValidate={!!control.validation}
+                        errorMessage={control.errorMessage}
+                        onChange={event => this.changeHandler(event.target.value, controlName)}
+                    />
+                    { index === 0 ? <hr /> : null }
+                </>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>QuizCreator</h1>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Создание теста</h1>
+
+                    <form omSubmit={this.submitHandler}>
+
+                        {this.renderInputes()}
+
+                        <select></select>
+
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}                  
+                        >
+                            Добавить вопрос
+                        </Button>
+
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}                  
+                        >
+                            Создать тест
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }    
